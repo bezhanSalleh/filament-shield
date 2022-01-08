2f9b11ec4cbfc5afc4df94d6396c75f1f27cb772
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2022-01-08 20:35:44 -0300

    Add Brazilian Portuguese translation

---
+++ resources/lang/pt_BR/filament-shield.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Table Columns
+    |--------------------------------------------------------------------------
+    */
+
+    'column.name' => 'Nome',
+    'column.guard_name' => 'Guard',
+    'column.permissions' => 'Permissões',
+    'column.updated_at' => 'Alterado em',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Form Fields
+    |--------------------------------------------------------------------------
+    */
+
+    'field.name' => 'Nome',
+    'field.guard_name' => 'Guard',
+    'field.permissions' => 'Permissões',
+    'field.select_all.name' => 'Selecionar todos',
+    'field.select_all.message' => 'Habilitar todas as permissões para essa função',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Filament Shield',
+    'nav.label' => 'Funções',
+    'nav.icon' => 'heroicon-o-shield-check',
+    'resource.label.role' => 'Função',
+    'resource.label.roles' => 'Funções',
+];
