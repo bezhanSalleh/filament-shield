13a1ca8cddc767ae4b8cf2ad32f6a2d92f99d14f
Bezhan Salleh <bezhan_salleh@yahoo.com>
2022-01-08 23:28:42 +0430

    view role resource page added

---
+++ stubs/resources/RoleResource/Pages/ViewRole.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Shield\RoleResource\Pages;
+
+use Filament\Resources\Pages\ViewRecord;
+use App\Filament\Resources\Shield\RoleResource;
+
+class ViewRole extends ViewRecord
+{
+    protected static string $resource = RoleResource::class;
+}
