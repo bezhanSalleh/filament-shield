78c3de4003a3a34e17a5c95ce215fd5e6a8d9916
Pathros Ibarra <patrotsky@yahoo.com>
2022-02-16 01:07:48 +0000

    Add Spanish translations

---
+++ resources/lang/es/filament-shield.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Table Columns
+    |--------------------------------------------------------------------------
+    */
+    
+    'column.name' => 'Nombre',
+    'column.guard_name' => 'Guard',
+    'column.roles' => 'Roles',
+    'column.permissions' => 'Permisos',
+    'column.updated_at' => 'Actualizado el',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Form Fields
+    |--------------------------------------------------------------------------
+    */
+
+    'field.name' => 'Nombre',
+    'field.guard_name' => 'Guard',
+    'field.permissions' => 'Permisos',
+    'field.select_all.name' => 'Seleccionar todos',
+    'field.select_all.message' => 'Habilitar todos los permisos para este rol',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Filament Shield',
+    'nav.role.label' => 'Roles',
+    'nav.role.icon' => 'heroicon-o-shield-check',
+    'resource.label.role' => 'Rol',
+    'resource.label.roles' => 'Roles',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Section & Tabs
+    |--------------------------------------------------------------------------
+    */
+    'section' => 'Entidades',
+    'resources' => 'Recursos',
+    'widgets' => 'Widgets',
+    'pages' => 'Páginas',
+    'custom' => 'Permisos personalizados',
+
+    /**
+     * HasPageShield Trait Forbidden Message
+     */
+    'forbidden' => 'No tiene permiso de accesso',
+];
+
