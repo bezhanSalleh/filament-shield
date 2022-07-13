56368f26de0fd30e1ac3ea558c4b72b79987ae13
Luis Arce <luilliarcec@gmail.com>
2022-07-13 14:35:06 -0500

    Adapt logic from command create shield

--- src/Commands/MakeCreateShieldCommand.php
+++ src/Commands/MakeCreateShieldCommand.php
@@ -2,8 +2,11 @@
 
 namespace BezhanSalleh\FilamentShield\Commands;
 
+use BezhanSalleh\FilamentShield\Contracts\HasPermissions;
 use BezhanSalleh\FilamentShield\FilamentShield;
+use Filament\Facades\Filament;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class MakeCreateShieldCommand extends Command
 {
@@ -11,36 +14,70 @@ class MakeCreateShieldCommand extends Command
     use Concerns\CanManipulateFiles;
     use Concerns\CanValidateInput;
 
-    public $signature = 'shield:create {name?}';
+    public $signature = 'shield:create {resource?}';
 
     public $description = 'Create Permissions and/or Policy for the given Filament Resource Model';
 
     public function handle(): int
     {
-        $model = $this->generateModelName($this->argument('name') ?? $this->askRequired('Model (e.g. `Post or PostResource`)', 'name'));
+        $resource = $this->argument('resource')
+            ?? $this->askRequired('Resource (e.g. `Filament\PostResource or PostResource`)', 'resource');
+
+        if (!Str::contains($resource, '\\')) {
+            $resource = collect(Filament::getResources())
+                ->first(fn($item) => Str::endsWith($item, $resource));
+        }
+
+        if (!class_exists($resource)) {
+            $this->error("A resource with the name '{$resource}' could not be found.");
+
+            return static::INVALID;
+        }
+
+        $model = class_basename($resource::getModel());
 
         $choice = $this->choice('What would you like to Generate for the Resource?', [
             "Permissions & Policy",
             "Only Permissions",
             "Only Policy",
-        ], 0, null, false);
+        ], 0);
 
         if ($choice === "Permissions & Policy") {
+            if (!$this->hasPermissions($resource)) {
+                $this->error(
+                    "The resource does not implement the 'HasPermissions' contract so it cannot generate the permissions."
+                );
+
+                return static::INVALID;
+            }
+
             if ($this->checkForCollision([$this->generatePolicyPath($model)])) {
                 return static::INVALID;
             }
 
-            $this->copyStubToApp('DefaultPolicy', $this->generatePolicyPath($model), $this->generatePolicyStubVariables($model));
+            $this->copyStubToApp(
+                'DefaultPolicy',
+                $this->generatePolicyPath($model),
+                $this->generatePolicyStubVariables($model)
+            );
 
             $this->info("Successfully generated {$model}Policy for {$model}Resource");
 
-            FilamentShield::generateForResource($model);
+            FilamentShield::generateForResource($model, $resource::permissions());
 
             $this->info("Successfully generated Permissions for {$model}Resource");
         }
 
         if ($choice === "Only Permissions") {
-            FilamentShield::generateForResource($model);
+            if (!$this->hasPermissions($resource)) {
+                $this->error(
+                    "The resource does not implement the 'HasPermissions' contract so it cannot generate the permissions."
+                );
+
+                return static::INVALID;
+            }
+
+            FilamentShield::generateForResource($model, $resource::permissions());
 
             $this->info("Successfully generated Permissions for {$model}Resource");
         }
@@ -50,11 +87,20 @@ class MakeCreateShieldCommand extends Command
                 return static::INVALID;
             }
 
-            $this->copyStubToApp('DefaultPolicy', $this->generatePolicyPath($model), $this->generatePolicyStubVariables($model));
+            $this->copyStubToApp(
+                'DefaultPolicy',
+                $this->generatePolicyPath($model),
+                $this->generatePolicyStubVariables($model)
+            );
 
             $this->info("Successfully generated {$model}Policy for {$model}Resource");
         }
 
         return self::SUCCESS;
+    }
+
+    protected function hasPermissions($resource): bool
+    {
+        return in_array(HasPermissions::class, class_implements($resource));
     }
 }
