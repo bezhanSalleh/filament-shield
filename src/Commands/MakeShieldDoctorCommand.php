6739bcdf3fa9fcdfe6e09b8abe0a8fbb10e80de0
Bezhan Salleh <bezhan_salleh@yahoo.com>
2022-07-20 08:10:59 +0430

    new command shield:doctor

---
+++ src/Commands/MakeShieldDoctorCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BezhanSalleh\FilamentShield\Commands;
+
+use BezhanSalleh\FilamentShield\FilamentShield;
+use Illuminate\Console\Command;
+
+class MakeShieldDoctorCommand extends Command
+{
+    public $signature = 'shield:doctor';
+
+    public $description = 'Create Permissions and/or Policy for the given Filament Resource Model';
+
+    public function handle(): int
+    {
+        $this->call('about',[
+            '--only' => 'filament_shield'
+        ]);
+
+        return self::SUCCESS;
+    }
+}
--- src/FilamentShieldServiceProvider.php
+++ src/FilamentShieldServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace BezhanSalleh\FilamentShield;
 
-use BezhanSalleh\FilamentShield\Models\Setting;
+use Illuminate\Support\Str;
+use Composer\InstalledVersions;
 use Filament\PluginServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Spatie\LaravelPackageTools\Package;
+use BezhanSalleh\FilamentShield\Models\Setting;
+use Illuminate\Foundation\Console\AboutCommand;
 
 class FilamentShieldServiceProvider extends PluginServiceProvider
 {
@@ -17,6 +20,7 @@ class FilamentShieldServiceProvider extends PluginServiceProvider
     {
         $package
             ->name('filament-shield')
+            ->hasConfigFile()
             ->hasTranslations()
             ->hasViews()
             ->hasCommands($this->getCommands())
@@ -50,15 +54,38 @@ class FilamentShieldServiceProvider extends PluginServiceProvider
         $this->publishes([
             $this->package->basePath("/../stubs/ShieldSettingSeeder.stub") => database_path('seeders/ShieldSettingSeeder.php'),
         ], "{$this->package->shortName()}-seeder");
+
+        if (class_exists(AboutCommand::class)) {
+            AboutCommand::add('Filament Shield', [
+                'Auth Provider' => config('filament-shield.auth_provider_model.fqcn'),
+                'Auth Provider Configured' => static::authProviderConfigured(),
+                'Settings Driver' => config('filament-shield.settings.driver'),
+                'Settings GUI' => config('filament-shield.settings.gui_enabled')
+                    ? '<fg=green;options=bold>ENABLED</>'
+                    : '<fg=red;options=bold>DISABLED</>',
+                'Translations' => is_dir(resource_path('resource/lang/vendor/filament-shield')) ? '<fg=red;options=bold>PUBLISHED</>' : '<fg=green;options=bold>NOT PUBLISHED</>',
+                'Version' => InstalledVersions::getPrettyVersion('bezhansalleh/filament-shield'),
+                'Views' => is_dir(resource_path('views/vendor/filament-shield')) ? '<fg=red;options=bold>PUBLISHED</>' : '<fg=green;options=bold>NOT PUBLISHED</>',
+            ]);
+        }
     }
 
     protected function getCommands(): array
     {
         return [
+            Commands\MakeShieldDoctorCommand::class,
             Commands\MakeCreateShieldCommand::class,
             Commands\MakeInstallShieldCommand::class,
             Commands\MakeGenerateShieldCommand::class,
             Commands\MakeSuperAdminShieldCommand::class,
         ];
+    }
+
+    protected static function authProviderConfigured()
+    {
+        return in_array("BezhanSalleh\FilamentShield\Traits\HasFilamentShield", class_uses(config('filament-shield.auth_provider_model.fqcn')))
+         || in_array("Spatie\Permission\Traits\HasRoles", class_uses(config('filament-shield.auth_provider_model.fqcn')))
+            ? '<fg=green;options=bold>YES</>'
+            : '<fg=red;options=bold>NO</>' ;
     }
 }
