3bb3be37f9d301ec6d14585db1b2deb0169edff6
Mendizalea <mendizalea@users.noreply.github.com>
2022-07-26 13:59:59 +0200

    Update filament-shield.php

--- resources/lang/es/filament-shield.php
+++ resources/lang/es/filament-shield.php
@@ -8,7 +8,7 @@ return [
     */
 
     'column.name' => 'Nombre',
-    'column.guard_name' => 'Guard',
+    'column.guard_name' => 'Nombre Guard',
     'column.roles' => 'Roles',
     'column.permissions' => 'Permisos',
     'column.updated_at' => 'Actualizado el',
@@ -20,9 +20,9 @@ return [
     */
 
     'field.name' => 'Nombre',
-    'field.guard_name' => 'Guard',
+    'field.guard_name' => 'Nombre Guard',
     'field.permissions' => 'Permisos',
-    'field.select_all.name' => 'Seleccionar todos',
+    'field.select_all.name' => 'Seleccionar todo',
     'field.select_all.message' => 'Habilitar todos los permisos actualmente <span class="text-primary font-medium">habilitados</span> para este rol',
 
     /*
@@ -42,32 +42,69 @@ return [
     | Section & Tabs
     |--------------------------------------------------------------------------
     */
+
     'section' => 'Entidades',
     'resources' => 'Recursos',
     'widgets' => 'Widgets',
     'pages' => 'Páginas',
     'custom' => 'Permisos personalizados',
 
-    /**
-     * Role Setting Page
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Shield Settings Page
+    |--------------------------------------------------------------------------
+    */
+
     'page' => [
-        'name' => 'Configuración',
+        'name' => 'Ajustes Shield',
         'icon' => 'heroicon-o-adjustments',
         'save' => 'Guardar',
         'generate' => 'Guardar y generar',
+        'load_default_settings' => 'Valores por defecto',
     ],
+
+    'settings' => [
+        'enabled' =>  false,
+        'label' => 'Página Ajustes',
+        'helper_text' => 'Habilitar/deshabilitar la página de Ajustes. Solo disponible para Super Admin.',
+        'navigation_label' => 'Ajustes',
+
+        'generator_options' => [
+            'policies_and_permissions' => 'Generar Politicas y Permisos',
+            'policies' => 'Generar solo Políticas',
+            'permissions' => 'Generar solo Permisos',
+        ],
+
+        'auth_provider' => [
+            'label' => 'Modelo Auth Provider',
+            'helper_text' => 'Nombre de clase completo del modelo utilizado para la generación de directivas.'
+        ],
+
+        'resource' => [
+            'name' => 'Recurso de Roles Shield',
+            'slug' => 'Slug',
+            'navigation_sort' => 'Ordenación de navegación',
+        ]
+    ],
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | MISC Labels
+    |--------------------------------------------------------------------------
+    */
+
     'labels.super_admin.toggle_input' => 'Rol de súper administrador',
     'labels.super_admin.text_input' => 'Nombre del rol',
     'labels.filament_user.toggle_input' => 'Rol de usuario Filament',
     'labels.filament_user.text_input' => 'Nombre del rol',
     'labels.role_policy.toggle_input' => '¿Política del rol registrada?',
-    'labels.role_policy.message' => 'Asegúrese que la política esté registrada y los permisos se apliquen',
-    'labels.prefixes.placeholder' => 'Prefijos de permiso por defecto',
-    'labels.prefixes.resource' => 'Recurso',
-    'labels.prefixes.resource.placeholder' => 'Agregar o eliminar permisos del recurso...',
-    'labels.prefixes.page' => 'Página',
-    'labels.prefixes.widget' => 'Widget',
+    'labels.role_policy.message' => 'Asegúrese que la política esté registrada y los permisos se apliquen.',
+    'labels.permission_prefixes.placeholder' => 'Prefijos de permiso por defecto',
+    'labels.permission_prefixes.resource' => 'Recurso',
+    'labels.permission_prefixes.resource.placeholder' => 'Agregar o eliminar permisos del recurso...',
+    'labels.permission_prefixes.page' => 'Página',
+    'labels.permission_prefixes.widget' => 'Widget',
     'labels.entities.placeholder' => 'Generadores de permisos de entidad y pestañas',
     'labels.entities.message' => 'Los generadores y pestañas son ',
     'labels.entities.resources' => 'Recursos',
@@ -79,7 +116,7 @@ return [
     'labels.status.disabled' => 'Deshabilitado',
     'labels.status.yes' => 'Sí',
     'labels.status.no' => 'No',
-    'labels.exclude.placeholder' => 'Modo exclusión',
+    'labels.exclude.placeholder' => 'Modo Exclusión',
     'labels.exclude.message' => 'La habilitación del Modo Exclusión le permite instruir al generador del permiso omitir la creación de permisos para las entidades que seleccione.',
     'labels.exclude.resources' => 'Recursos',
     'labels.exclude.resources.placeholder' => 'Selecionar recursos ...',
@@ -88,11 +125,34 @@ return [
     'labels.exclude.widgets' => 'Widgets',
     'labels.exclude.widgets.placeholder' => 'Seleccionar widgets ...',
 
-    /**
-     * Messages
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Messages
+    |--------------------------------------------------------------------------
+    */
+
     'forbidden' => 'Usted no tiene permiso de acceso',
     'update' => '¡La configuración del Shield ha sido renovada!',
     'generate' => '¡La configuración del Shield ha sido renovada y se han generado los permisos y/o políticas!',
+    'loaded_default_settings' => 'Ajustes predeterminados de Shield cargados.',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Resource Permissions' Labels
+    |--------------------------------------------------------------------------
+    */
+
+    'resource_permission_prefixes_labels' => [
+        'view' => 'View',
+        'view_any' => 'View Any',
+        'create' => 'Create',
+        'update' => 'Update',
+        'delete' => 'Delete',
+        'delete_any' => 'Delete Any',
+        'force_delete' => 'Force Delete',
+        'force_delete_any' => 'Force Delete Any',
+        'restore' => 'Restore',
+        'restore_any' => 'Restore Any',
+        'replicate' => 'Replicate',
+    ],
 ];
