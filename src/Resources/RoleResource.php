33b8f9773ed6826193ca7b38b4354648b4fbf9c2
bezhanSalleh <bezhanSalleh@users.noreply.github.com>
2022-08-03 17:02:35 +0000

    Fix

--- src/Resources/RoleResource.php
+++ src/Resources/RoleResource.php
@@ -2,21 +2,21 @@
 
 namespace BezhanSalleh\FilamentShield\Resources;
 
-use Closure;
-use Filament\Forms;
-use Filament\Tables;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
-use Filament\Resources\Form;
-use Filament\Resources\Table;
-use Filament\Resources\Resource;
-use Illuminate\Support\Collection;
-use Spatie\Permission\Models\Role;
-use Illuminate\Database\Eloquent\Model;
-use Spatie\Permission\Models\Permission;
-use BezhanSalleh\FilamentShield\Support\Utils;
 use BezhanSalleh\FilamentShield\FilamentShield;
 use BezhanSalleh\FilamentShield\Resources\RoleResource\Pages;
+use BezhanSalleh\FilamentShield\Support\Utils;
+use Closure;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RoleResource extends Resource
 {
@@ -363,6 +363,7 @@ class RoleResource extends Resource
         $entities = $record->permissions->pluck('name')
             ->reduce(function ($roles, $role) {
                 $roles[$role] = Str::afterLast($role, '_');
+
                 return $roles;
             }, collect())
             ->values()
@@ -380,7 +381,7 @@ class RoleResource extends Resource
             }, []);
 
         // set entity's state if one are all permissions are true
-        if (Arr::exists($entities, $entity) && Arr::get($entities,$entity)) {
+        if (Arr::exists($entities, $entity) && Arr::get($entities, $entity)) {
             $set($entity, true);
         } else {
             $set($entity, false);
