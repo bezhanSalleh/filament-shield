063a0ae5cb01fbe65ad5cd9ea57e4a1a14d1d5e4
a21ns1g4ts <atila.danvi@outlook.com>
2023-06-27 01:55:24 -0300

    Keep code estable

--- src/Resources/RoleResource/Pages/EditRole.php
+++ src/Resources/RoleResource/Pages/EditRole.php
@@ -4,13 +4,18 @@ namespace BezhanSalleh\FilamentShield\Resources\RoleResource\Pages;
 
 use BezhanSalleh\FilamentShield\Resources\RoleResource;
 use BezhanSalleh\FilamentShield\Resources\RoleResource\Pages\Concerns\SyncPermissions;
+use BezhanSalleh\FilamentShield\Support\Utils;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 class EditRole extends EditRecord
 {
     use SyncPermissions;
+
+    public Collection $permissions;
 
     protected static string $resource = RoleResource::class;
 
@@ -27,6 +32,11 @@ class EditRole extends EditRecord
         // $this->pages = $this->getPermissions($data, 'page');
         // $this->widgets = $this->getPermissions($data, 'widget');
 
+        // backwards compatibility provisory
+        $this->permissions = collect($data)->filter(function ($permission, $key) {
+            return ! in_array($key, ['name', 'guard_name', 'select_all']) && Str::contains($key, '_');
+        })->keys();
+
         return Arr::only($data, ['name', 'guard_name']);
     }
 
@@ -35,5 +45,16 @@ class EditRole extends EditRecord
         $this->syncPermissions($this->resources, 'resource');
         // $this->syncPermissions($this->pages, 'page');
         // $this->syncPermissions($this->widgets, 'widget');
+
+        // backwards compatibility provisory
+        $permissionModels = collect();
+        $this->permissions->each(function ($permission) use ($permissionModels) {
+            $permissionModels->push(Utils::getPermissionModel()::firstOrCreate([
+                'name' => $permission,
+                'guard_name' => $this->data['guard_name'],
+            ]));
+        });
+
+        $this->record->syncPermissions($permissionModels);
     }
 }
