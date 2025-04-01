5d120ecfab16134be95a44b8d3b047f8f32cbbbe
Panservice <154348101+panservicesas@users.noreply.github.com>
2025-04-01 02:17:16 +0200

    Update filament-shield.php
    Added sub_navigation_position config option

--- config/filament-shield.php
+++ config/filament-shield.php
@@ -7,6 +7,7 @@ return [
         'navigation_sort' => -1,
         'navigation_badge' => true,
         'navigation_group' => true,
+        'sub_navigation_position' => null,
         'is_globally_searchable' => false,
         'show_model_path' => true,
         'is_scoped_to_tenant' => true,
